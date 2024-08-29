68d46e5b543dec9c89abb4f5b604ed57314fe3ae
Ali <farokhniaali900@gmail.com>
2024-08-29 15:25:39 +0330

    model created

--- books/models.py
+++ books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class book(models.Model):
+    title = models.CharField(
+        max_length=200,
+        null=False,
+    )
+    author = models.CharField(
+        max_length=200,
+        null=True,
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+    publication_date = models.DateField()
+
+    def __str__(self):
+        return self.title
